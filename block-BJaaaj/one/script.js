aead1848ae0c819014684c26ab863bd4e03e7cff
Akhilesh <akhileshburathoki@gmail.com>
2020-11-08 15:33:52 +0530

    JS ex-2

---
+++ block-BJaaaj/one/script.js
@@ -0,0 +1,14 @@
+let firstName="Arya"
+let lastName="Stark"
+alert(firstName)
+alert(lastName)
+firstName="John"
+lastName="Snow"
+alert(firstName + " " + lastName)
+const fullName=firstName + "" + lastName
+let name="Akhilesh"
+let newUser="Akhilesh"
+let isAdmin="true"
+alert(name)
+alert(newUser)
+alert(isAdmin)
