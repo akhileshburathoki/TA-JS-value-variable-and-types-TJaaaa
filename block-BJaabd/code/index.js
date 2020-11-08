754ba4d747c677330aa0f47082f4ad1eb4aa4375
Akhilesh <akhileshburathoki@gmail.com>
2020-11-08 15:47:04 +0530

    JS ex-7

--- block-BJaabd/code/index.js
+++ block-BJaabd/code/index.js
@@ -1,88 +1,53 @@
-// Condition
-// Write your code below each problems:
+let user=prompt(`Your name`)
+alert(user)
+let userName=(user)
+console.log (user, userName)
+var age=22
+, isLearning=true;
+user=(`john`)
+console.log (user, userName)
+let num1=prompt(`Write a number`)
+let num2=prompt(`Write another number`)
+let sum=(num1 + num2)
+alert(sum)
 
-/*
-Write a program that asks the user his/her age and check for the following conditions :
+for(let j=0; j<=10; j = j + 2) {
+    console.log(j)
+}
+for(let a=0;a<=10;a=a + 1){
+    if(a % 2 !== 0 ){
+        alert(a)
+    }
+}
+let i=5;
+let final = ""
 
-- `if` the age is between 12-55 then print the message "You can participate in the marathon".
-- `if` the age is between 4-11 then print the message " You are too young to participate in the marathon".
-- `if` the age is less than 4 then print the message " Hey Kiddo! Can You Walk ?"
-- `if` the age is greater than 55 then print the message " You are too old to participate in the marthon".
-👇
-*/
+while(i >= 0){
+    final = final + i;
+    i = i - 1;
+}
+alert(final);
 
-// Loops
-/*
-Given a positive integer `n`. Print the word (hello) in format of heeeello (letter 'e' must be repeated `n` times). Take input from prompt and print the result in alert.
+let b=10;
+ final = "";
+while(b >= 0){
+    if(b % 2 == 0){
+        final = final + b;
+    }
+    b = b - 1;
+}
+alert(final)
+console.log (final)
 
-Example:
-n = 1 => output: hello
-n = 7 => output: heeeeeeello
-👇
-*/
-// [Your code goes here]
 
-/*
-Program to calculate the sum of first n natural numbers(1,2,3...n are known as natural numbers). Prompt user to enter n (using `prompt`) then based on input provided calculate and show result in `alert`.
-👇
-*/
-// [Your code goes here]
 
-/* Switch Statement
 
- 🎖Using switch statement do the following
 
-Take a number value from user and alert the message if it matches the conditions.
-* [ ] ONE, if `number` is equal to 1.
-* [ ] TWO, if `number` is equal to 2.
-* [ ] THREE, if `number` is equal to 3.
-* [ ] FOUR, if `number` is equal to 4.
-* [ ] FIVE, if `number` is equal to 5.
-* [ ] SIX, if `number` is equal to 6.
-* [ ] SEVEN, if `number` is equal to 7.
-* [ ] EIGHT, if `number` is equal to 8.
-* [ ] NINE, if `number` is equal to 9.
-* [ ] PLEASE TRY AGAIN, if  is none of the above.
 
-*/
-// [Your code goes here]
 
-/*
-🎖Using switch statement do the following
 
-Take the value of `marks` (0-100) from user using `prompt` and `alert` the message (Your Grade is AA) as giver below.
-* [ ] `AA` if `marks` is greater than 90.
-* [ ] `AB` if `marks` is greater than 80 and less than or equal to 90
-* [ ] `BB` if `marks` is greater than 70 and less than or equal to 80
-* [ ] `BC` if `marks` is greater than 60 and less than or equal to 70
-* [ ] `CC` if `marks` is greater than 50 and less than or equal to 60
-* [ ] `CD` if `marks` is greater than 40 and less than or equal to 50
-* [ ] `DD` if `marks` is greater than 30 and less than or equal to 40
-* [ ] `FF` if `marks` is less than or equal to 30
-*/
 
-// [Your code goes here]
 
-/*
- 🎖Write a JavaScript program that takes two `integers` from user (using prompt) and alerts the larger number.
-*/
-// [your code goes here]
 
-/*
-🎖Write a JavaScript conditional statement to find the sign (+, -) of product of three numbers. Take those three numbers from user using `prompt`. Display an alert box with the specified sign.
-*/
-// [Your code goes here]
 
-/* Calculator
- 🎖Make a simple calculator with these functions. Using prompt, type conversion, if else statement. Use prompt to take the input from user i.e two numbers and an operation (Add, Sub, Mul, Div).
 
-  ⛑ Rule
-    * [ ] While substracting and dividing keep in mind the number one should be greater then number two. If not show alert saying `Number Two is larger then Number one`.
-  ⚡️ Operations
-    * [ ] Add
-    * [ ] Sub
-    * [ ] Mul
-    * [ ] Div
-*/
-
-// [Your code goes here]
