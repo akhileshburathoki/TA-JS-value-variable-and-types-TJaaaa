f71fd3c6740ffd46676234af2e73e9bb6135bcfe
Akhilesh <akhileshburathoki@gmail.com>
2020-11-08 15:44:19 +0530

    JS ex-5

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -1,57 +1,170 @@
-// if..else
+let num=prompt("Please write any number");
+if (num % 2 == 0) {
+    alert(`It's a even number`);
+}else {
+    alert(`It's a odd number`);
+}
+let num1=prompt("write a number")
+let num2=prompt("write another number")
+if (num1 > num2) {
+    alert(num1);
+}else {
+    alert(num2);
+}
+let houseName=prompt("House name")
+let name1=("Arya")
+let arya=("Winter is coming")
+let name2=("Lannister");
+let lannister=("A lannister always pays his debt")
+let all=("All men must die")
+if (houseName == name1) {
+    alert(arya);
+}else if(houseName == name2) {
+    alert(lannister);
+}else {
+    alert(all)
+}
+let month=prompt("Write any month and we will tell you how many days it holds")
+let january=("january")
+let march=("march")
+let may=("may")
+let july=("july")
+let august=("august")
+let october=("oct0ber")
+let dec=("december")
 
-/*
-1. Write a program that asks the user for a number and check the following conditions:
+let febuary=("febuary")
+let april=("april")
+let june=("june")
+let sep=("september")
+let nov=("november")
 
-  - `if` the number is even print the message " number is even"
-  - `if` the number is odd print the message "number is odd"
-*/
+switch (month) {
+    case january:
+    case march:
+    case may:
+    case july:
+    case august:
+    case october:
+    case dec:
+        alert("31")    
+        break;
+    case april:
+    case june:
+    case sep:
+    case nov:
+        alert("30");
+        break;
+    case febuary:
+        alert("28 and 29 on leap years");
+        break;
+    default:
+        alert("Error")
+}
+let salery=prompt("Tell your salery")
+switch (true) {
+    case salery > 20000:
+        alert(salery / 10)
+        break;
+    case salery < 40000:
+        alert(salery / 20);
+        break;
+    case salery > 50000:
+        alert(salery / 30)
+        break;
+    default:
+        console.log("salery, default", salery)
+        alert("do not support this much amount")
+}
+let marks=prompt("Your marks")
+if (marks > 100) {
+    alert(`Marks can't be greater than 100`)
+}else if(marks > 80) {
+    alert("Grade A")
+}else if(marks > 50) {
+    alert("Grade B")
+}else if (marks > 30)  {
+    alert("Grade C")
+}else if (marks > 0) {
+    alert("Grade D")
+}
 
-// 2. Write a program to accept two numbers from user using`prompt` and alert the max value.
+switch (true) {
+    case marks > 100:
+        alert(`Marks can't be greater than 100`)
+        break;
+    case marks > 80:
+        alert("Grade A")
+        break;
+    case marks > 50:
+        alert("Grade B")
+        break;
+    case marks > 30:
+        alert("Grade C")
+        break;
+    case marks > 0:
+        alert("Grade D")
+        break;
+    default:
+        alert('error')
+        break;
+}
 
-// 3. Convert the above code using`?` terniary operator
+let weather=prompt("What is the weather like?")
+let sun=(`sunny`)
+let sunny=(`Wear a t-shirt`)
+let rain=(`rainy`)
+let rainy=(`Don't forget to take your raincoat`)
+let hot=(`hot`)
+let hanky=(`Get a hanky`)
+let cold=(`freezing`)
+let freezing=(`Get your sweater on`)
 
-/*
-4. Write a program that asks the user for the house name and check the following conditions:
+if (weather == sun) {
+    alert(sunny)
+}else if(weather==rain){
+    alert(rainy)
+}else if(weather==hot){
+    alert(hanky)
+}else if (weather==cold){
+    alert(freezing)
+}else{
+    alert("Invalid input")
+}
 
-  - `if` house name is "stark" then print the message " Winter is coming"
-  - `if` house name is "lannister" then print the message " A lannister always pays his debt"
-  - `else` print the message " All men must die"
-*/
+switch (weather) {
+    case sun:
+        alert(sunny)
+        break;
+    case rain:
+        alert(rainy)
+        break;
+    case hot:
+        alert(hanky)
+        break;
+    case cold:
+        alert(freezing)
+    default:
+        alert("Invalid input")
 
-// 5. Convert the above code using`?` terniary operator
+}
 
-// Switch
 
-// 6. Write a program that takes the number of the month(1 - 12) and alert number of days in the month.
 
-/* 7.
-- Write a program that take the salery of the user using prompt and alert the in -hand amount.You will find out the in -hand amount by deducting the tax amoun from salery.Conditions are given below.
-  - `Salary <= 20000` tax is 10 %
-  - `Salary <= 40000` tax is 20 %
-  - `Salary > 50000` tax is 30 %
 
-*/
 
-//  if..else vs switch
 
-/* 8.
-Implement the condition give below using`if..else` and`switch` statement.
 
-  - `marks > 100` alert`"Marks can't be greater than 100"`
-  - `marks > 80 && marks < 100` alert`"Grade A"`
-  - `marks > 50 && marks < 80` alert`"Grade B"`
-  - `marks > 30 && marks < 50` alert`"Grade C"`
-  - `marks > 0` alert`"Grade D"`
 
-*/
 
-/* 9. Weather app
 
-  - Ask user to provide the condition of the weather outside by asking `What is the weather like outside?`
-  - If user says`sunny` alert`Wear a T-shirt`
-  - If`rainy` alert`Don't forget to take your raincoat`
-  - If`hot` alert`Get a hanky`
-  - If`freezing` alert`Get your sweeter on`
-  - Anything else should alert`Not a valid input`
-*/
+
+
+
+
+
+
+
+
+
+
