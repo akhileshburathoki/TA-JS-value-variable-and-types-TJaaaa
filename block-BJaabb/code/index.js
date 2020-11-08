30fc89870de825dd0dcb18a88de101bcbee0ae74
Akhilesh <akhileshburathoki@gmail.com>
2020-11-08 17:40:52 +0530

    JS ex-6

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -1,38 +1,40 @@
-/* 1. Do the following
+let user=prompt(`Your name`)
+alert(user)
+let userName=(user)
+console.log (user, userName)
+var age=22
+, isLearning=true;
+user=(`john`)
+console.log (user, userName)
+let num1=prompt(`Write a number`)
+let num2=prompt(`Write another number`)
+let sum=(num1 + num2)
+alert(sum)
 
-1. Declare a variable `user` and take the name using `prompt`. Display the value stored in `user` using `alert`.
-2. Now copy the value of `user` into a new variable named `userName`. Log the value of both `user` and `userName` using `console.log`.
-3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
-4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
-*/
+for(let j=0; j<=10; j = j + 2) {
+    console.log(j)
+}
+for(let a=0;a<=10;a=a + 1){
+    if(a % 2 !== 0 ){
+        alert(a)
+    }
+}
+let i=5;
+let final = ""
 
-/* 2.
-Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
-*/
+while(i >= 0){
+    final = final + i;
+    i = i - 1;
+}
+alert(final);
 
-// Loops
-
-// 3. Complete the following code to make the output be 0 2 4 6 8 10?
-
-for (let j = 0; j <= 10; ____) console.log(j);
-
-// 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
-
-// 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
-
-// 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
-
-// Comparisoin
-
-/* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
-
-- `alert` true or false base on the entered value.
-- if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
-
-
-Example:
-
-21, 21; // true
-"hello", -21; // false
-
-*/
+let b=10;
+ final = "";
+while(b >= 0){
+    if(b % 2 == 0){
+        final = final + b;
+    }
+    b = b - 1;
+}
+alert(final)
+console.log (final)
